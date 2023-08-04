a38645fe1ec89f0aae7911332c89ce4f3a605f94
Chanely Marques <chanely.marques@dell.com>
2023-08-04 16:37:07 -0300

    chore: creating dataframes to be exported

--- sqlalchemy/load_data.py
+++ sqlalchemy/load_data.py
@@ -5,8 +5,14 @@ import pandas as pd
 
 from database.connector import engine
 
+# I needed to SET GLOBAL FOREIGN_KEY_CHECKS=0; in order to load the csv files.
+# Load the files using the sequence above:
+
 #categories = pd.read_csv('data/categories.csv', sep=',',quotechar='\'',encoding='utf8').set_index('name')
 #categories.to_sql('category', con=engine, if_exists='append')
+
+#products = pd.read_csv('data/products.csv', sep=',',quotechar='\'',encoding='utf8', skiprows=[4]).set_index('price')
+#products.to_sql('product', con=engine, if_exists='append')
 
 #customers = pd.read_csv('data/customers.csv', sep=',',quotechar='\'',encoding='utf8').set_index('email')
 #customers.to_sql('customer', con=engine, if_exists='append')
@@ -14,8 +20,5 @@ from database.connector import engine
 #items = pd.read_csv('data/items.csv', sep=',', quotechar='\'', encoding='utf8')
 #items.to_sql('item', con=engine, if_exists='append', index=False)
 
-orders = pd.read_csv('data/orders.csv', sep=',', quotechar='\'', encoding='utf8')
-orders.to_sql('orders', con=engine, if_exists='append', index=False)
-
-#products = pd.read_csv('data/products.csv', sep=',',quotechar='\'',encoding='utf8', skiprows=[4]).set_index('price')
-#products.to_sql('product', con=engine, if_exists='append')
+#orders = pd.read_csv('data/orders.csv', sep=',', quotechar='\'', encoding='utf8')
+#orders.to_sql('orders', con=engine, if_exists='append', index=False)
--- sqlalchemy/queries.py
+++ sqlalchemy/queries.py
@@ -1,11 +1,26 @@
 import sys
 sys.path.insert(0, '')
 
-from sqlalchemy import text
+import pandas as pd
 
-from database.connector import session
+from database.connector import engine
 
-# Use this result to check database connection
-# Need to create logging function
-result = session.execute(text('SHOW INDEXES FROM alchemy.*;'))
-print(result.fetchall())
+tables = ['category', 'customer', 'item', 'orders', 'product']
+
+def createDataFrame(table):
+    table_query = f"SELECT \
+        TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, COLUMN_KEY, NUMERIC_PRECISION, NUMERIC_SCALE \
+            FROM INFORMATION_SCHEMA.COLUMNS \
+                WHERE table_name = '{table}' \
+                    AND table_schema = 'alchemy'"
+
+    table_df = pd.DataFrame(pd.read_sql_query(table_query, con=engine))
+    return table_df
+
+customer_df = createDataFrame("customer")
+item_df = createDataFrame("item")
+orders_df = createDataFrame("orders")
+product_df = createDataFrame("product")
+
+data = pd.concat([category_df, customer_df, item_df, orders_df, product_df])
+print(data)
